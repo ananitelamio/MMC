88543df52dc044046ca616302bc450bf05b3121b
Your Name <you@example.com>
2020-08-03 22:08:01 +0100

    update appjs

--- src/App.js
+++ src/App.js
@@ -85,24 +85,24 @@ import { css } from "styled-components/macro"; //eslint-disable-line
 /* Ready Made Pages (from demos folder) */
 // import EventLandingPage from "demos/EventLandingPage.js";
 // import HotelTravelLandingPage from "demos/HotelTravelLandingPage.js";
-// import AgencyLandingPage from "demos/AgencyLandingPage.js";
+import AgencyLandingPage from "demos/AgencyLandingPage.js";
 // import SaaSProductLandingPage from "demos/SaaSProductLandingPage.js";
 // import RestaurantLandingPage from "demos/RestaurantLandingPage.js";
 // import ServiceLandingPage from "demos/ServiceLandingPage.js";
 // import HostingCloudLandingPage from "demos/HostingCloudLandingPage.js";
 
 /* Inner Pages */
-// import LoginPage from "pages/Login.js";
-// import SignupPage from "pages/Signup.js";
+import LoginPage from "pages/Login.js";
+import SignupPage from "pages/Signup.js";
 // import PricingPage from "pages/Pricing.js";
-// import AboutUsPage from "pages/AboutUs.js";
-// import ContactUsPage from "pages/ContactUs.js";
+import AboutUsPage from "pages/AboutUs.js";
+import ContactUsPage from "pages/ContactUs.js";
 // import BlogIndexPage from "pages/BlogIndex.js";
-// import TermsOfServicePage from "pages/TermsOfService.js";
-// import PrivacyPolicyPage from "pages/PrivacyPolicy.js";
+import TermsOfServicePage from "pages/TermsOfService.js";
+import PrivacyPolicyPage from "pages/PrivacyPolicy.js";
 
-import ComponentRenderer from "ComponentRenderer.js";
-import MainLandingPage from "MainLandingPage.js";
+//import ComponentRenderer from "ComponentRenderer.js";
+//import MainLandingPage from "MainLandingPage.js";
 
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
@@ -111,14 +111,26 @@ export default function App() {
   return (
     <Router>
       <Switch>
-        <Route path="/components/:type/:subtype/:name">
-          <ComponentRenderer />
+        <Route path="/login">
+          <LoginPage />
         </Route>
-        <Route path="/components/:type/:name">
-          <ComponentRenderer />
+        <Route path="/sign-up">
+          <SignupPage />
         </Route>
-        <Route path="/">
-          <MainLandingPage />
+        <Route path="/about-us">
+          <AboutUsPage />
+        </Route>
+        <Route path="/terms">
+          <TermsOfServicePage />
+        </Route>
+        <Route path="/privacy">
+          <PrivacyPolicyPage />
+        </Route>
+        <Route path="/contact">
+          <ContactUsPage />
+        </Route>
+        <Route exact path="/">
+          <AgencyLandingPage />
         </Route>
       </Switch>
     </Router>
