2ae226de74841614e2f63435144654e51e7406bb
Destino Dello <destinodello@gmail.com>
2020-08-03 21:33:55 +0000

    added Loginjs

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,21 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
+import { useFormik } from 'formik';
+import * as Yup from 'yup';
 import AnimationRevealPage from "helpers/AnimationRevealPage.js";
 import { Container as ContainerBase } from "components/misc/Layouts";
 import tw from "twin.macro";
 import styled from "styled-components";
 import {css} from "styled-components/macro"; //eslint-disable-line
-import illustration from "images/login-illustration.svg";
+import illustration from "images/undraw_access_account_99n5.svg";
 import logo from "images/logo.svg";
 import googleIconImageSrc from "images/google-icon.png";
 import twitterIconImageSrc from "images/twitter-icon.png";
 import { ReactComponent as LoginIcon } from "feather-icons/dist/icons/log-in.svg";
 
-const Container = tw(ContainerBase)`min-h-screen bg-primary-900 text-white font-medium flex justify-center -m-8`;
+const Container = tw(ContainerBase)`min-h-screen bg-blue-600 text-white font-medium flex justify-center -m-8`;
 const Content = tw.div`max-w-screen-xl m-0 sm:mx-20 sm:my-16 bg-white text-gray-900 shadow sm:rounded-lg flex justify-center flex-1`;
 const MainContainer = tw.div`lg:w-1/2 xl:w-5/12 p-6 sm:p-12`;
 const LogoLink = tw.a``;
-const LogoImage = tw.img`h-12 mx-auto`;
-const MainContent = tw.div`mt-12 flex flex-col items-center`;
+const LogoImage = tw.img`h-40 mx-auto`;
+const MainContent = tw.div`flex flex-col items-center`;
 const Heading = tw.h1`text-2xl xl:text-3xl font-extrabold`;
 const FormContainer = tw.div`w-full flex-1 mt-8`;
 
@@ -38,8 +40,9 @@ const DividerText = tw.div`leading-none px-2 inline-block text-sm text-gray-600 
 
 const Form = tw.form`mx-auto max-w-xs`;
 const Input = tw.input`w-full px-8 py-4 rounded-lg font-medium bg-gray-100 border border-gray-200 placeholder-gray-500 text-sm focus:outline-none focus:border-gray-400 focus:bg-white mt-5 first:mt-0`;
+const Error = tw.div`text-red-400 text-sm`;
 const SubmitButton = styled.button`
-  ${tw`mt-5 tracking-wide font-semibold bg-primary-500 text-gray-100 w-full py-4 rounded-lg hover:bg-primary-900 transition-all duration-300 ease-in-out flex items-center justify-center focus:shadow-outline focus:outline-none`}
+  ${tw`mt-5 tracking-wide font-semibold bg-blue-600 text-gray-100 w-full py-4 rounded-lg hover:bg-blue-800 transition-all duration-300 ease-in-out flex items-center justify-center focus:shadow-outline focus:outline-none`}
   .icon {
     ${tw`w-6 h-6 -ml-2`}
   }
@@ -53,10 +56,25 @@ const IllustrationImage = styled.div`
   ${tw`m-12 xl:m-16 w-full max-w-sm bg-contain bg-center bg-no-repeat`}
 `;
 
+const validationSchema = Yup.object().shape({
+  email: Yup
+    .string()
+    .email()
+    .required("Email is a required field"),
+  currentPassword: Yup
+    .string()
+    .required("Please enter your password")
+});
+
+const initialValues = {
+  email: "",
+  currentPassword: ""
+};
+
 export default ({
   logoLinkUrl = "#",
   illustrationImageSrc = illustration,
-  headingText = "Sign In To Treact",
+  headingText = "Sign In To My Mobile Cash",
   socialButtons = [
     {
       iconImageSrc: googleIconImageSrc,
@@ -71,10 +89,23 @@ export default ({
   ],
   submitButtonText = "Sign In",
   SubmitButtonIcon = LoginIcon,
-  forgotPasswordUrl = "#",
-  signupUrl = "#",
+  forgotPasswordUrl = "https://mymobilecash.themoneytransferapplication.com/forgot-password.xhtml",
+  signupUrl = "/sign-up",
 
-}) => (
+}) => {
+  const formik = useFormik({
+        initialValues,
+        validationSchema,
+        onSubmit: values => {
+            console.log(JSON.stringify(values, null, 2));
+        }
+    });
+
+    
+    const emailProps = formik.getFieldProps("email");
+    const currentPasswordProps = formik.getFieldProps("currentPassword");
+
+    return (
   <AnimationRevealPage>
     <Container>
       <Content>
@@ -85,22 +116,13 @@ export default ({
           <MainContent>
             <Heading>{headingText}</Heading>
             <FormContainer>
-              <SocialButtonsContainer>
-                {socialButtons.map((socialButton, index) => (
-                  <SocialButton key={index} href={socialButton.url}>
-                    <span className="iconContainer">
-                      <img src={socialButton.iconImageSrc} className="icon" alt=""/>
-                    </span>
-                    <span className="text">{socialButton.text}</span>
-                  </SocialButton>
-                ))}
-              </SocialButtonsContainer>
-              <DividerTextContainer>
-                <DividerText>Or Sign in with your e-mail</DividerText>
-              </DividerTextContainer>
-              <Form>
-                <Input type="email" placeholder="Email" />
-                <Input type="password" placeholder="Password" />
+              <Form onSubmit={formik.handleSubmit}>
+                <Input type="email" placeholder="Email" name="email" {...emailProps} />
+                {formik.touched.email && formik.errors.email ? (<Error>{formik.errors.email}</Error>): null}
+
+                <Input type="password" placeholder="Password" name="currentPassword" {...currentPasswordProps} />
+                {formik.touched.currentPassword && formik.errors.currentPassword ? (<Error>{formik.errors.currentPassword}</Error>): null}
+
                 <SubmitButton type="submit">
                   <SubmitButtonIcon className="icon" />
                   <span className="text">{submitButtonText}</span>
@@ -125,5 +147,5 @@ export default ({
         </IllustrationContainer>
       </Content>
     </Container>
-  </AnimationRevealPage>
-);
+  </AnimationRevealPage>)
+};
