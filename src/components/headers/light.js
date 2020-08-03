09c86dd92462124240aafe870c8b4f838025aea5
Destino Dello <destinodello@gmail.com>
2020-08-03 21:56:08 +0000

    added lightjs

--- src/components/headers/light.js
+++ src/components/headers/light.js
@@ -11,8 +11,8 @@ import { ReactComponent as MenuIcon } from "feather-icons/dist/icons/menu.svg";
 import { ReactComponent as CloseIcon } from "feather-icons/dist/icons/x.svg";
 
 const Header = tw.header`
-  flex justify-between items-center
-  max-w-screen-xl mx-auto
+  flex justify-between items-center pt-4 
+  max-w-screen-xl mx-auto 
 `;
 
 export const NavLinks = tw.div`inline-block`;
@@ -23,21 +23,20 @@ export const NavLinks = tw.div`inline-block`;
 export const NavLink = tw.a`
   text-lg my-2 lg:text-sm lg:mx-6 lg:my-0
   font-semibold tracking-wide transition duration-300
-  pb-1 border-b-2 border-transparent hover:border-primary-500 hocus:text-primary-500
+  pb-1 border-b-2 text-yellow-500 border-transparent hover:border-blue-500 hocus:text-blue-500
 `;
 
 export const PrimaryLink = tw(NavLink)`
   lg:mx-0
-  px-8 py-3 rounded bg-primary-500 text-gray-100
-  hocus:bg-primary-700 hocus:text-gray-200 focus:shadow-outline
+  px-8 py-3 rounded bg-blue-500 text-gray-100
+  hocus:bg-blue-700 hocus:text-gray-200 focus:shadow-outline
   border-b-0
 `;
 
 export const LogoLink = styled(NavLink)`
   ${tw`flex items-center font-black border-b-0 text-2xl! ml-0!`};
-
   img {
-    ${tw`w-10 mr-3`}
+    ${tw`h-32 mr-3`}
   }
 `;
 
@@ -72,14 +71,13 @@ export default ({ roundedHeaderButton = false, logoLink, links, className, colla
    */
   const defaultLinks = [
     <NavLinks key={1}>
-      <NavLink href="/#">About</NavLink>
-      <NavLink href="/#">Blog</NavLink>
-      <NavLink href="/#">Pricing</NavLink>
-      <NavLink href="/#">Contact Us</NavLink>
-      <NavLink href="/#" tw="lg:ml-12!">
+      <NavLink href="/">Home</NavLink>
+      <NavLink href="/about-us">About Us</NavLink>
+      <NavLink href="/contact">Contact Us</NavLink>
+      <NavLink href="/login" tw="lg:ml-12!">
         Login
       </NavLink>
-      <PrimaryLink css={roundedHeaderButton && tw`rounded-full`}href="/#">Sign Up</PrimaryLink>
+      <PrimaryLink css={roundedHeaderButton && tw`rounded-full`}href="/sign-up">Sign Up</PrimaryLink>
     </NavLinks>
   ];
 
@@ -89,7 +87,6 @@ export default ({ roundedHeaderButton = false, logoLink, links, className, colla
   const defaultLogoLink = (
     <LogoLink href="/">
       <img src={logo} alt="logo" />
-      Treact
     </LogoLink>
   );
 
