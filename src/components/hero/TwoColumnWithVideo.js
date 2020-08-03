6af38a94aafa9a0b76f16b0b5de5b519f8e27b2f
Destino Dello <destinodello@gmail.com>
2020-08-03 21:41:50 +0000

    added TwoColumnVideo

--- src/components/hero/TwoColumnWithVideo.js
+++ src/components/hero/TwoColumnWithVideo.js
@@ -7,26 +7,35 @@ import { css } from "styled-components/macro";
 import Header from "../headers/light.js";
 
 import ReactModalAdapter from "../../helpers/ReactModalAdapter.js";
+import Quote from "../forms/Quote.js";
 import ResponsiveVideoEmbed from "../../helpers/ResponsiveVideoEmbed.js";
 
-import { ReactComponent as PlayIcon } from "feather-icons/dist/icons/play-circle.svg";
 import { ReactComponent as CloseIcon } from "feather-icons/dist/icons/x.svg";
 import { ReactComponent as SvgDecoratorBlob1 } from "../../images/svg-decorator-blob-1.svg";
 import { ReactComponent as SvgDecoratorBlob2 } from "../../images/dot-pattern.svg";
-import DesignIllustration from "../../images/design-illustration.svg";
+import DesignIllustration from "../../images/undraw_youtube_tutorial_2gn3.svg";
 
-const Container = tw.div`relative`;
+
+const Container = styled.div`
+    ${tw`relative -mx-8 -mt-8 bg-center bg-cover`}
+    background-image: url("https://images.unsplash.com/photo-1522071901873-411886a10004?ixlib=rb-1.2.1&auto=format&fit=crop&w=1920&q=80");
+`;
+
+const OpacityOverlay = tw.div`z-10 absolute inset-0 bg-blue-100 opacity-25`;
+
+const HeroContainer = tw.div`z-20 relative px-4 sm:px-8 max-w-screen-xl mx-auto`;
+
 const TwoColumn = tw.div`flex flex-col lg:flex-row md:items-center max-w-screen-xl mx-auto py-20 md:py-24`;
 const LeftColumn = tw.div`relative lg:w-6/12 lg:pr-12 flex-shrink-0 text-center lg:text-left`;
-const RightColumn = tw.div`relative mt-12 lg:mt-0 flex flex-col justify-center`;
+const RightColumn = tw.div`w-full sm:w-5/6 lg:w-1/2 mt-16 lg:mt-0 lg:pl-8`;
 
-const Heading = tw.h1`font-black text-3xl md:text-5xl leading-snug max-w-3xl`;
-const Paragraph = tw.p`my-5 lg:my-8 text-sm lg:text-base font-medium text-gray-600 max-w-lg mx-auto lg:mx-0`;
+const Heading = tw.h1`font-black text-3xl md:text-5xl leading-snug max-w-3xl text-yellow-600`;
+const Paragraph = tw.p`my-5 lg:my-8 text-sm lg:text-base font-medium text-gray-200 max-w-lg mx-auto lg:mx-0`;
 
 const Actions = tw.div`flex flex-col items-center sm:flex-row justify-center lg:justify-start mt-8`;
-const PrimaryButton = tw.button`font-bold px-8 lg:px-10 py-3 rounded bg-primary-500 text-gray-100 hocus:bg-primary-700 focus:shadow-outline focus:outline-none transition duration-300`;
+const PrimaryButton = tw.button`font-bold px-8 lg:px-10 py-3 rounded bg-blue-500 text-gray-100 hocus:bg-blue-700 focus:shadow-outline focus:outline-none transition duration-300`;
 const WatchVideoButton = styled.button`
-  ${tw`mt-4 sm:mt-0 sm:ml-8 flex items-center text-secondary-300 transition duration-300 hocus:text-primary-400 focus:outline-none`}
+  ${tw`mt-4 sm:mt-0 sm:ml-8 flex items-center text-gray-200 transition duration-300 hocus:text-blue-500 focus:outline-none`}
   .playIcon {
     ${tw`stroke-1 w-12 h-12`}
   }
@@ -50,21 +59,33 @@ const StyledModal = styled(ReactModalAdapter)`
     ${tw`fixed inset-0 z-50`}
   }
   &.mainHeroModal__content {
-    ${tw`xl:mx-auto m-4 sm:m-16 max-w-screen-xl absolute inset-0 flex justify-center items-center rounded-lg bg-gray-200 outline-none`}
+    ${tw`xl:mx-auto m-4 sm:m-16 max-w-screen-sm absolute inset-0 flex justify-center items-center rounded-lg bg-gray-200 outline-none`}
   }
   .content {
     ${tw`w-full lg:p-16`}
   }
 `;
+
+const Notification = tw.span`inline-block my-4 pl-3 py-1 border-l-4 border-blue-500 font-medium text-sm text-gray-200`;
+
 const CloseModalButton = tw.button`absolute top-0 right-0 mt-8 mr-8 hocus:text-primary-500`;
 
+const StyledResponsiveVideoEmbed = styled(ResponsiveVideoEmbed)`
+  padding-bottom: 56.25% !important;
+  padding-top: 0px !important;
+  ${tw`rounded`}
+  iframe {
+    ${tw`rounded bg-black shadow-xl`}
+  }
+`;
+
 export default ({
-  heading = "Modern React Templates, Just For You",
- description="Our templates are easy to setup, understand and customize. Fully modular components with a variety of pages and components.",
+  heading = "My Mobile Cash Your closest partner",
+  description="We provide fast, cheap and secure national and international transfers. Your money travels in the blink of an eye and automatically the beneficiary is paid.",
   primaryButtonText="Get Started",
   primaryButtonUrl="#",
   watchVideoButtonText="Watch Video",
-  watchVideoYoutubeUrl="https://www.youtube.com/embed/_GuOjXYl5ew",
+  watchVideoYoutubeUrl="https://www.youtube.com/embed/BL22vTGteQU",
   imageSrc=DesignIllustration,
   imageCss=null,
   imageDecoratorBlob = false,
@@ -75,33 +96,28 @@ export default ({
 
   return (
     <>
-      <Header />
+      
       <Container>
-        <TwoColumn>
-          <LeftColumn>
-            <Heading>{heading}</Heading>
-            <Paragraph>{description}</Paragraph>
-            <Actions>
-              <PrimaryButton as="a" href={primaryButtonUrl}>{primaryButtonText}</PrimaryButton>
-              <WatchVideoButton onClick={toggleModal}>
-                <span className="playIconContainer">
-                  <PlayIcon className="playIcon" />
-                </span>
-                <span className="playText">{watchVideoButtonText}</span>
-              </WatchVideoButton>
-            </Actions>
-          </LeftColumn>
-          <RightColumn>
-            <IllustrationContainer>
-              <img
-                css={imageCss}
-                src={imageSrc}
-                alt="Hero"
-              />
-              {imageDecoratorBlob && <DecoratorBlob2 />}
-            </IllustrationContainer>
-          </RightColumn>
-        </TwoColumn>
+        <OpacityOverlay />
+        <HeroContainer>
+            <Header />
+            <TwoColumn>
+                <LeftColumn>
+                    <Notification>A dedicated and dynamic team at your service</Notification>
+                    <Heading>{heading}</Heading>
+                    <Paragraph>{description}</Paragraph>
+                    <Actions>
+                    <PrimaryButton as="a" href={primaryButtonUrl} onClick={toggleModal}>{primaryButtonText}</PrimaryButton>
+                    </Actions>
+                </LeftColumn>
+                <RightColumn>
+                    <StyledResponsiveVideoEmbed
+                        url="https://www.youtube.com/embed/BL22vTGteQU"
+                        background="transparent"
+                    />
+                </RightColumn>
+            </TwoColumn>
+        </HeroContainer>
         <DecoratorBlob1 />
         <StyledModal
           closeTimeoutMS={300}
@@ -114,7 +130,7 @@ export default ({
             <CloseIcon tw="w-6 h-6" />
           </CloseModalButton>
           <div className="content">
-            <ResponsiveVideoEmbed url={watchVideoYoutubeUrl} tw="w-full" />
+              <Quote/>
           </div>
         </StyledModal>
       </Container>
