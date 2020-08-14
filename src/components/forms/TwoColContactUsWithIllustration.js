77eb34de221542d24ff4d545a7bef53e9364cb7e
Destino Dello <destinodello@gmail.com>
2020-08-14 15:00:47 +0000

    Subscribe

--- src/components/forms/TwoColContactUsWithIllustration.js
+++ src/components/forms/TwoColContactUsWithIllustration.js
@@ -1,4 +1,6 @@
 import React from "react";
+import { useFormik } from 'formik';
+import * as Yup from 'yup';
 import tw from "twin.macro";
 import styled from "styled-components";
 import { css } from "styled-components/macro"; //eslint-disable-line
@@ -28,21 +30,53 @@ const Description = tw.p`mt-4 text-center md:text-left text-sm md:text-base lg:t
 const Form = tw.form`mt-8 md:mt-10 text-sm flex flex-col lg:flex-row`
 const Input = tw.input`border-2 px-5 py-3 rounded focus:outline-none font-medium transition duration-300 hocus:border-blue-400`
 
+const Error = tw.div`text-red-400 text-sm`;
+
 const SubmitButton = tw(PrimaryButtonBase)`inline-block lg:ml-6 mt-6 lg:mt-0`
+
+const validationSchema = Yup.object().shape({
+  email: Yup
+    .string()
+    .email()
+    .required("Email is a required field")
+});
 
 const hubspot = require('@hubspot/api-client');
 const hubspotClient = new hubspot.Client({ apiKey: '16239a52-1612-4736-9d47-735929ad6217', basePath: 'https://cors-anywhere.herokuapp.com/https://api.hubapi.com/'});
+
+const initialValues = {
+  email: ""
+};
+
+const onSubmit = (data) => {
+
+    console.log(data);
+    
+    hubspotClient.crm.contacts.basicApi.create({ properties: { email: data.email , subscribed: "subscribed" } })
+    .then(response => {
+        console.log(response);
+      })
+      .catch(e => {
+        console.log(e);
+      });
+};
 
 export default ({
   subheading = "Stay up to date",
   heading = <>Feel free to <span tw="text-primaryOrange-500">Subscribe </span>and stay informed!<wbr/></>,
   description = "Stay informed about our latest services and innovations to serve you better.",
   submitButtonText = "I subscribe",
-  formAction = "#",
-  formMethod = "get",
-  textOnLeft = true,
+  textOnLeft = true
 }) => {
   // The textOnLeft boolean prop can be used to display either the text on left or right side of the image.
+    const formik = useFormik({
+        initialValues,
+        validationSchema,
+        onSubmit
+    });
+
+    const emailProps = formik.getFieldProps("email");
+
 
   return (
     <Container>
@@ -55,8 +89,9 @@ export default ({
             {subheading && <Subheading>{subheading}</Subheading>}
             <Heading>{heading}</Heading>
             <Description>{description}</Description>
-            <Form action={formAction} method={formMethod}>
-              <Input type="email" name="email" placeholder="Your Email Address" />
+            <Form onSubmit={formik.handleSubmit}>
+              <Input type="email" name="email" placeholder="Your Email Address" {...emailProps}/>
+                {formik.touched.email && formik.errors.email ? (<Error>{formik.errors.email}</Error>): null}
               <SubmitButton type="submit">{submitButtonText}</SubmitButton>
             </Form>
           </TextContent>
